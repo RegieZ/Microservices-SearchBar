46e54d234e5ad35e824f06c96e9beead76162b44
zhaozirui <806262720@qq.com>
2020-08-05 15:55:19 +0800

    整合swagger2自动生成接口文档，接口文档中会声明接口的请求地址、请求参数类型以及响应参数等

---
+++ system-service/src/main/java/com/regino/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.regino.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.regino.system"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("后台管理系统")
+                        .description("后台管理系统，详细信息......")
+                        .version("1.0")
+                        /* .contact(new Contact("Reggie","https://blog.csdn.net/Regino","regiezale@163.com"))
+                        .license("The Apache License")
+                        .licenseUrl("http://www.baidu.com")*/
+                        .build());
+    }
+}
--- system-service/src/main/java/com/regino/controller/SystemController.java
+++ system-service/src/main/java/com/regino/controller/SystemController.java
@@ -3,6 +3,8 @@ package com.regino.controller;
 import com.regino.pojo.Question;
 import com.regino.service.SystemService;
 import com.regino.util.IDWorker;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("system")
+@Api(tags = "添加问题controller")
 public class SystemController {
 
     @Qualifier("search-service")
     @Autowired
     private SystemService systemService;
 
+    @ApiOperation("添加问题接口")
     @PostMapping("/addQuestion")
     public String addQuestion(@RequestBody Question question) {
         IDWorker idWorker = new IDWorker(0, 0);
