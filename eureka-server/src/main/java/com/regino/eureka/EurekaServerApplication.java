a4199f01b283073c0aa5d1994ce9a12da243179f
zhaozirui <806262720@qq.com>
2020-08-05 14:05:14 +0800

    Eureka Server

--- eureka-server/src/main/java/com/regino/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/regino/eureka/EurekaServerApplication.java
@@ -1,8 +1,10 @@
-package com.regino;
+package com.regino.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
