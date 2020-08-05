0bffac4a0e68c0bcda1b9d7912584f083d061177
zhaozirui <806262720@qq.com>
2020-08-05 14:37:26 +0800

    启动类开启Eureka客户端&Feign支持

--- search-service/src/main/java/com/regino/SearchServiceApplication.java
+++ search-service/src/main/java/com/regino/SearchServiceApplication.java
@@ -2,8 +2,10 @@ package com.regino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //声明是Eureka客户端
 public class SearchServiceApplication {
 
     public static void main(String[] args) {
--- system-service/src/main/java/com/regino/SystemServiceApplication.java
+++ system-service/src/main/java/com/regino/SystemServiceApplication.java
@@ -2,8 +2,17 @@ package com.regino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/*
+    @EnableDiscoveryClient 和 @EnableEurekaClient
+        共同点：都是能够让注册中心能够发现，扫描到该服务
+        不同点：@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient可以是其他注册中心（eureka/zk）
+ */
 @SpringBootApplication
+@EnableDiscoveryClient //声明是Eureka客户端
+@EnableFeignClients //开启feign支持
 public class SystemServiceApplication {
 
     public static void main(String[] args) {
