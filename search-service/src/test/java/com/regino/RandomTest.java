e40fcbeaf668cd9fb44a07832dbb9d2b2f0fd980
zhaozirui <806262720@qq.com>
2020-08-06 15:38:49 +0800

    Add RandomTest

---
+++ search-service/src/test/java/com/regino/RandomTest.java
@@ -0,0 +1,31 @@
+package com.regino;
+
+import java.util.*;
+
+public class RandomTest {
+
+    public static void main(String[] args) {
+        while(true) {
+            //组1
+            List<String> list1 = new ArrayList<>();
+            Collections.addAll(list1, "Adam", "Ben", "Carl");
+
+            //组2
+            List<String> list2 = new ArrayList<>();
+            Collections.addAll(list2, "Eve", "Barbie", "Candy");
+
+            Map<Integer, List<String>> map = new HashMap<>();
+            map.put(1, list1);
+            map.put(2, list2);
+
+            Random random = new Random();
+            //获取组序号
+            Scanner scanner = new Scanner(System.in);
+            int groupNum = scanner.nextInt();
+            List<String> list = map.get(groupNum);
+            //抽取LuckyDog
+            String str = list.get(random.nextInt(list.size()));
+            System.out.println(str);
+        }
+    }
+}
