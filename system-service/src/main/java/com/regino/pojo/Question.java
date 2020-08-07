caaf0e3a49fb5abdaf309f3dd655f67e44458f31
zhaozirui <806262720@qq.com>
2020-08-07 15:11:07 +0800

    优化API Models(Question)

--- system-service/src/main/java/com/regino/pojo/Question.java
+++ system-service/src/main/java/com/regino/pojo/Question.java
@@ -1,14 +1,20 @@
 package com.regino.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel
 public class Question {
 
-    private Long id;
-
+    @ApiModelProperty(value = "id")
+    private String id;
+    //问题
+    @ApiModelProperty(value = "问题")
     private String title;
-
+    //答案
+    @ApiModelProperty(value = "答案")
     private String answer;
-
 }
+
